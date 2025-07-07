0675da946d66bd6fa99d9d5d8714d54b35c12fc9
Sunmibare Idowu <sunmijewel@gmail.com>
2025-07-07 16:07:24 +0100

    correlated and non-correlated subqueries.

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,19 @@
+-- 1. Non-Correlated Subquery: Find all properties where average rating > 4.0
+SELECT *
+FROM properties
+WHERE id IN (
+  SELECT property_id
+  FROM reviews
+  GROUP BY property_id
+  HAVING AVG(rating) > 4.0
+);
+
+
+-- 2. Correlated Subquery: Find users who have made more than 3 bookings
+SELECT *
+FROM users u
+WHERE (
+  SELECT COUNT(*)
+  FROM bookings b
+  WHERE b.user_id = u.id
+) > 3;
