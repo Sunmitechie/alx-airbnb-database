c7201914c6a4ef1492405b0bc2788188ec241a96
Sunmibare Idowu <sunmijewel@gmail.com>
2025-07-07 16:26:42 +0100

    Using SQL aggregation and window functions to analyze data.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,9 +5,18 @@ GROUP BY user_id
 ORDER BY total_bookings DESC;
 
 
--- 2. Rank properties by total bookings using ROW_NUMBER()
+-- 2a. Rank properties by total bookings using ROW_NUMBER()
 SELECT property_id, COUNT(*) AS total_bookings,
        ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_num
 FROM bookings
 GROUP BY property_id
 ORDER BY row_num;
+
+
+-- 2b. Rank properties by total bookings using RANK()
+SELECT property_id, COUNT(*) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id
+ORDER BY rank;
+
